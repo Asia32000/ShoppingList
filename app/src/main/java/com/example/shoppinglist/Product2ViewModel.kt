e03ed0dd915632b30c84d78126b69e1549d3d00a
Asia32000 <89982291+Asia32000@users.noreply.github.com>
2023-12-15 19:40:10 +0100

    Create Product2ViewModel.kt

--- app/src/main/java/com/example/shoppinglist/Product2ViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/Product2ViewModel.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
 import com.google.firebase.Firebase
 import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
@@ -14,8 +13,6 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.google.firebase.database.getValue
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 
 class Product2ViewModel(val app: Application, val products2: SnapshotStateList<Product2>) : AndroidViewModel(app) {
 
